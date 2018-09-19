1f4db3420c327c61cef9664ea6b8ecf2c19e8c06
Danni <38550500+isdanni@users.noreply.github.com>
2018-09-19 14:25:01 +0800

    Update newau.py

--- newau.py
+++ newau.py
@@ -1,37 +1,23 @@
-import pandas as pd
+from __future__ import print_function
+
 import numpy as np
-from sklearn import datasets, linear_model
-from sklearn.model_selection import train_test_split
-from matplotlib import pyplot as plt
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, RobustScaler
 
-
+from keras.layers import Input, Dense
 from keras.models import Model
-from keras.layers import Input
-from keras.layers import LSTM
-from keras.layers import Dense
-from keras.utils.vis_utils import plot_model
+from sklearn.model_selection import train_test_split
 
-
-
-df= pd.read_csv('/home/danni/Dropbox/CS/Code/gene_autoencoder/human_time.csv'
-                 , header=None)
-df= df.iloc[1:]  # drop first header
-
-
-df['zero_each_row'] = df.apply( lambda s : s.value_counts().get(0,0), axis=1) # get number of zeros in each row
-total_cols = len(df.colamns) -1
-df['zero_percentage'] = df['zero_each_row']/total_cols
-df = df[df['zero_percentage'] <= 0.75]
-
-df.loc["Total"] = df.sum()
-
-
+# use Matplotlib (don't ask)
+import matplotlib.pyplot as plt
+import time
 
 # this is the size of our encoded representations
 encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
 
 # this is our input placeholder
 input_img = Input(shape=(784,))
+
 # "encoded" is the encoded representation of the input
 encoded = Dense(encoding_dim, activation='relu')(input_img)
 # "decoded" is the lossy reconstruction of the input
@@ -50,4 +36,52 @@ decoder_layer = autoencoder.layers[-1]
 # create the decoder model
 decoder = Model(encoded_input, decoder_layer(encoded_input))
 
+
 autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+
+data = pd.read_csv("../input/train.csv",header=0)
+x_data = data.values[:,1:]
+y_data = data.label
+x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size = 0.3)
+
+
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+print(x_train.shape)
+print(x_test.shape)
+
+
+autoencoder.fit(x_train, x_train,
+                epochs=50,
+                batch_size=256,
+                shuffle=True,
+                validation_data=(x_test, x_test))
+
+
+# encode and decode some digits
+# note that we take them from the *test* set
+encoded_imgs = encoder.predict(x_test)
+decoded_imgs = decoder.predict(encoded_imgs)
+
+
+
+n = 10  # how many digits we will display
+plt.figure(figsize=(20, 4))
+for i in range(n):
+    # display original
+    ax = plt.subplot(2, n, i + 1)
+    plt.imshow(x_test[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # display reconstruction
+    ax = plt.subplot(2, n, i + 1 + n)
+    plt.imshow(decoded_imgs[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.show()
