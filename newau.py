242553d833b35f1d73779545bc5713bdd2789db3
Danni <38550500+isdanni@users.noreply.github.com>
2018-09-19 13:46:37 +0800

    Update newau.py

--- newau.py
+++ newau.py
@@ -25,4 +25,29 @@ df = df[df['zero_percentage'] <= 0.75]
 
 df.loc["Total"] = df.sum()
 
-print(df)
+
+
+# this is the size of our encoded representations
+encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
+
+# this is our input placeholder
+input_img = Input(shape=(784,))
+# "encoded" is the encoded representation of the input
+encoded = Dense(encoding_dim, activation='relu')(input_img)
+# "decoded" is the lossy reconstruction of the input
+decoded = Dense(784, activation='sigmoid')(encoded)
+
+# this model maps an input to its reconstruction
+autoencoder = Model(input_img, decoded)
+
+# this model maps an input to its encoded representation
+encoder = Model(input_img, encoded)
+
+# create a placeholder for an encoded (32-dimensional) input
+encoded_input = Input(shape=(encoding_dim,))
+# retrieve the last layer of the autoencoder model
+decoder_layer = autoencoder.layers[-1]
+# create the decoder model
+decoder = Model(encoded_input, decoder_layer(encoded_input))
+
+autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
